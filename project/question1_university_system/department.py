1892cc3a2ca2933884e39e3fb7a55d6b1493f8e7
chamika <chamika@softvilmedia.com>
2025-09-06 11:27:18 +0530

    department class added

---
+++ project/question1_university_system/department.py
@@ -0,0 +1,11 @@
+
+# =======================================================
+# File: department.py
+# Created by: CD
+# Date: 2025-09-06
+# =======================================================
+
+
+class Department:
+    def __init__(self, name):
+        self.name = name
