14b2e1a520f12a0cca9c6d8ec9d5e4c96045ef80
chamika <chamika@softvilmedia.com>
2025-09-17 13:29:04 +0530

    retry with exponential backoff

--- project/question2_social_media_analysis/data_collection/data/manager/http_client_imp.py
+++ project/question2_social_media_analysis/data_collection/data/manager/http_client_imp.py
@@ -1,16 +1,31 @@
 import aiohttp
+import asyncio
 from domain.manager.ihhtp_client import IHttpClient
 from domain.model.api_response import ApiResponse
 
 class HttpClientImp(IHttpClient):
-    def __init__(self, timeout = 3):
+    def __init__(self, timeout = 3, maxRetry = 3):
         self.timeout = timeout
+        self.maxRetries = maxRetry
+        self.backOff = 0.2
 
     async def get(self, url: str) -> ApiResponse:
-        try:
-            async with aiohttp.ClientSession(timeout=aiohttp.ClientTimeout(total=self.timeout)) as session:
-                async with session.get(url) as response:
-                    text = await response.text()
-                    return ApiResponse(status_code=response.status, body=text)
-        except Exception as e:
-            return ApiResponse(status_code=-1, body=str(e))
+        return await self._retry_async("url")
+        
+    async def _request(self, url: str) -> ApiResponse:
+        async with aiohttp.ClientSession(timeout=aiohttp.ClientTimeout(total=self.timeout)) as session:
+            async with session.get(url) as response:
+                text = await response.text()
+                return ApiResponse(status_code=response.status, body=text)
+        
+    async def _retry_async(self, url: str):
+        attempt = 0
+        while attempt < self.maxRetries:
+            try:
+                return await self._request(url)
+            except Exception as e:
+                attempt += 1
+                slepTime = min(self.backOff * (2 ** (attempt - 1)), 2.0)
+                print(f"Request Failed!, Attempt: {attempt}/{self.maxRetries} after {slepTime} secs")
+                await asyncio.sleep(slepTime)
+        return ApiResponse(status_code=-1, body=f"Failed with max retry: {self.maxRetries}")
